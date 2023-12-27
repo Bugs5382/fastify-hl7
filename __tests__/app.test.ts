121a95d1bcd2b02966f17308191b8cbc016e49b2
“Bugs5382” <bugs5382@users.noreply.github.com>
2023-12-27 17:40:28 -0500

    test: server, route sends to server

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,9 +1,8 @@
-
 import fastify, { FastifyInstance } from 'fastify'
 import portfinder from 'portfinder'
 import tcpPortUsed from 'tcp-port-used'
 import fastifyHL7 from '../src'
-import { expectEvent } from './__utils__/utils'
+import {createDeferred, expectEvent, sleep} from './__utils__/utils'
 
 let app: FastifyInstance
 
@@ -61,6 +60,59 @@ describe('fastify-hl7 sample app tests', () => {
     await expectEvent(OB, 'connect')
 
     await OB.close()
+    await IB.close()
+  })
+
+  test('...start server, connect with client - via route', async () => {
+    await app.register(fastifyHL7)
+
+    const dfd = createDeferred<void>() // eslint-disable-line
+
+    app.get('/process/request', async function (_request, reply) {
+      const hl7Object = new app.hl7.CreateMessage({
+        messageHeader: {
+          msh_9_1: 'ADT',
+          msh_9_2: 'A01',
+          msh_11_1: 'D'
+        }
+      })
+
+      const client = new app.hl7.CreateClient({ host: '0.0.0.0' })
+      const OB = client.createOutbound({ port: LISTEN_PORT }, async (resInbound) => {
+        const messageRes = resInbound.getMessage()
+        expect(messageRes.get('MSA.1').toString() === 'AA').toBe(true)
+        dfd.resolve() // unit test done... continue
+      })
+
+      await OB.sendMessage(hl7Object)
+
+      await OB.close()
+
+      return await reply.send({result: true});
+    })
+
+    // server
+    const server = new app.hl7.CreateServer()
+    const IB = server.createInbound({ port: LISTEN_PORT }, async (_req, res) => {
+      // if you are looking at this as an example, the _req object has the message being sent by the client
+      await res.sendResponse('AA') // send a message that we are good.
+    })
+
+    await expectEvent(IB, 'listen')
+
+    await sleep(5)
+
+    const { body } = await app.inject({
+      method: 'GET',
+      url: '/process/request'
+    })
+
+    await dfd.promise
+
+    const { result } = JSON.parse(body)
+
+    expect(result).toBe(true)
+
     await IB.close()
   })
 })
