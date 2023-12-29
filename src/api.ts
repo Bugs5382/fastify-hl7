6ba74e40fc8557571cc9dd2a22daa4e9c8af6301
“Bugs5382” <bugs5382@users.noreply.github.com>
2023-12-29 15:26:04 -0500

    docs: updated api

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,9 @@
 import { HL7 } from 'fastify'
 import fastifyHL7 from '.'
 
-import { FastifyHL7Options } from './decorate.js'
+import { AServers, FastifyHL7Options } from './decorate.js'
 import { HL7Client } from './class/hL7Client.js'
 import { HL7Server } from './class/hL7Server.js'
 
 export default fastifyHL7
-export { fastifyHL7, HL7, FastifyHL7Options, HL7Client, HL7Server }
+export { fastifyHL7, HL7, FastifyHL7Options, HL7Client, HL7Server, AServers }
