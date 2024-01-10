c4bf6aa2e0ae51cc1011d218657b6b888be65e8d
“Bugs5382” <bugs5382@users.noreply.github.com>
2024-01-10 09:04:45 -0500

    chore: remove release channel [ci skip]

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,6 @@ module.exports = {
   extends: '@the-rabbit-hole/semantic-release-config',
   branches: [
     'main',
-    { name: 'develop', prerelease: 'beta' },
-    { name: 'release', prerelease: 'rc' }
+    { name: 'develop', prerelease: 'beta' }
   ]
 }
